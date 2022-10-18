14dc797e23b60d28670d9f2b2c02875ebf8aa7e8
STS668-PAVANKUMAR P <STS668-PAVANKUMAR P@STS668L-PAVANKU>
2022-10-18 18:41:20 +0530

    Add project files.

---
+++ WebSite/Controllers/PavanController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class PavanController : Controller
+    {
+        // GET: Pavan
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        public ActionResult LoginValidation(Paper v)
+        {
+            if (v.UserName == "pavan" && v.Password == "abc")
+            {
+                ViewBag.s = "Hai";
+                ViewData["dev"] = "good Morning";
+                Paper employee = new Paper()
+                {
+                    EmployeeId = 101,
+                    EmployeeName = "Pavan kumar",
+                    EmployeeCompany = "Span Technology",
+                    EmployeePhoneNo = 9989786705,
+                    EmployeeLocation = "Chennai"
+                };
+                return View ("Info", employee);
+            }
+            else
+            {
+                ViewBag.x = "Invalid Username and Password! Please Try again.";
+                return View("Login");
+            }
+            
+          
+        }
+        //public ActionResult Info()
+        //{
+        //    Paper employee = new Paper()
+        //    {
+        //        EmployeeId = 101,
+        //        EmployeeName = "Pavan kumar",
+        //        EmployeeCompany = "Span Technology",
+        //        EmployeePhoneNo = 9989786705,
+        //        EmployeeLocation = "Chennai"
+        //    };
+        //    return View(employee);
+        //}
+    }
+}
---
+++ WebSite/Models/Paper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+    public class Paper
+    { 
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeCompany { get; set; }
+        public long EmployeePhoneNo { get; set; }
+
+        public string EmployeeLocation { get; set; }
+    }
+}
